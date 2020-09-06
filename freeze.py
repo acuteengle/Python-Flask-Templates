3ed2165f6da50ec0f46af29ad73aabb13bd34bd8
Brandon Eng <beng713@gmail.com>
2020-09-06 10:56:35 -0700

    frozen flask

---
+++ freeze.py
@@ -0,0 +1,8 @@
+from flask_frozen import Freezer
+from myapp import app
+
+# https://medium.com/@francescaguiducci/how-to-build-a-simple-personal-website-with-python-flask-and-netlify-d800c97c283d
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    freezer.freeze()
